50a8c44f700b2b50dd76a2d387cc06857c10c00e
Sheeban Shaikh <shaikhsheebanz@gmail.com>
2016-08-14 16:37:36 -0700

    updated main file

--- src/Application.java
+++ src/Application.java
@@ -1,17 +1,8 @@
-import memberManagement.models.MemberDetails;
-import memberManagement.models.MemberType;
-import memberManagement.service.Customer;
-import memberManagement.service.Driver;
-import memberManagement.service.Member;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.env.SystemEnvironmentPropertySource;
 import rideManagement.FinalizeRide;
 import rideManagement.ProcessRide;
 import rideManagement.WaitingRide;
-import routingManagement.ProcessRouting;
 
-import java.util.*;
+import java.util.Scanner;
 
 /**
  * Created by sheebanshaikh on 8/12/16.
@@ -22,7 +13,7 @@ public class Application {
 
     public static void main(String args[]) {
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         WaitingRide  Wait_obj = new WaitingRide();
         ProcessRide  proc_Req = new ProcessRide();
         FinalizeRide final_req = new FinalizeRide();
@@ -30,7 +21,7 @@ public class Application {
         Scanner s = new Scanner(System.in);
         b: do
         {
-            int ch = mainMenu(s);
+            int ch = mainMenu();
             switch (ch)
             {
                 case 1:
@@ -41,33 +32,23 @@ public class Application {
                     signInMenu(s);
                     c:do
                     {
-                        int ch1 = bookRideUpdateMenu(s);
-                        switch(ch1)
-                        {
-                            case 1:  System.out.println("1)Book a Ride\n"+"2)Update Details\n"+"3)Sign out");
-                                System.out.println("Enter Your Choice");
-                                Scanner sc =new Scanner(System.in);
-                                int ch2= sc.nextInt();
-                                System.out.println(ch2);
-                                switch(ch2)
+                        int ch1 = bookRideUpdateMenu();
+
+                        switch (ch1)
                                 {
                                     case 1:
-                                        sourceAndDestinationMenu(sc);
+                                        sourceAndDestinationMenu();
                                         continue c;
 
                                     case 2:
-                                        updateDetailsMenu(sc);
+                                        updateDetailsMenu();
                                         break;
 
-                                    case 3: continue b;
+                                    case 3:// continue b;
 
                                 }
                                 break;
 
-
-                            case 3: continue b;
-
-                        }
                     } while(true);
 
                 case 3: return;
@@ -80,20 +61,22 @@ public class Application {
 
     }
 
-    private static int bookRideUpdateMenu(Scanner s) {
+    private static int bookRideUpdateMenu() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("1)Book a Ride\n"
                 +"2)Update Details\n"
                 + "3)Sign Out"
         );
         System.out.println("Enter Your Choice");
-        return s.nextInt();
+        return scanner.nextInt();
     }
 
-    private static int mainMenu(Scanner s) {
+    private static int mainMenu() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("======================= Online Taxi Booking ======================");
         System.out.println("1)Sign up\n2)Sign In\n3)Exit");
         System.out.println("Enter the Choice: ");
-        return s.nextInt();
+        return scanner.nextInt();
     }
 
     private static void signInMenu(Scanner s) {
@@ -125,26 +108,28 @@ public class Application {
         String Exp_date =  s.next();
     }
 
-    private static void updateDetailsMenu(Scanner sc) {
+    private static void updateDetailsMenu() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your new password: ");
-        String curr_pw = sc.next();
+        String curr_pw = scanner.next();
         System.out.println("Confirm your new password: ");
-        String con_pass =sc.next();
+        String con_pass = scanner.next();
         System.out.println("Enter the Card type: "
                 + "1)Credit\n"
                 + "2)Debit");
-        String card_type_up = sc.next();
+        String card_type_up = scanner.next();
         System.out.println("Enter Your card Number: ");
-        int card_num_up = sc.nextInt();
+        int card_num_up = scanner.nextInt();
         System.out.println("Enter the Expiration Date ");
-        String Exp_date_up =  sc.next();
+        String Exp_date_up = scanner.next();
     }
 
-    private static void sourceAndDestinationMenu(Scanner sc) {
+    private static void sourceAndDestinationMenu() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the Source Address: ");
-        String source = sc.next();
+        String source = scanner.next();
         System.out.println("Enter the destination Address");
-        String destination= sc.next();
+        String destination = scanner.next();
     }
 
 
