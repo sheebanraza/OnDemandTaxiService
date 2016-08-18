3345312324eeb494fa7367da01f41325d281eb90
Sheeban Shaikh <shaikhsheebanz@gmail.com>
2016-08-17 19:31:30 -0700

    updated track ride

--- src/trackride/TrackRide.java
+++ src/trackride/TrackRide.java
@@ -3,5 +3,42 @@ package trackride;
 /**
  * Created by sheebanshaikh on 8/16/16.
  */
-public class TrackRide {
+
+
+public class TrackRide implements TrackStatus {
+
+    String position;
+
+    Integer gpsLoc;
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Integer getGpsLoc() {
+        return gpsLoc;
+    }
+
+    public void setGpsLoc(Integer gpsLoc) {
+        this.gpsLoc = gpsLoc;
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Performing Updations");
+    }
+
+    @Override
+    public void onStartNotify() {
+        System.out.println("Performing Start Notifications");
+    }
+
+    @Override
+    public void onEndNotify() {
+        System.out.println("Performing End Notifications");
+    }
 }
